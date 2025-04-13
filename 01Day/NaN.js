0e0696433bf44b8932b8cd48e25aa53f39cb0123
Jaya Singh <sv.jayasingh@gmail.com>
2025-04-13 23:47:11 +0530

    using NaN

---
+++ 01Day/NaN.js
@@ -0,0 +1,12 @@
+function sanitize(x) {
+  if (isNaN(x)) {
+    return NaN;
+  }
+  return x;
+}
+
+console.log(sanitize("1"));
+// Expected output: "1"
+
+console.log(sanitize("NotANumber"));
+// Expected output: NaN
